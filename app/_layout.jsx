cb42d17884aba19b5d19e1647fe5af447e21a218
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2024-08-08 11:07:48 +0530

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,30 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import LoginScreen from './../components/LoginScreen'
+
+export default function RootLayout() {
+  useFonts({
+    'outfit' : require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium' : require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold' : require('./../assets/fonts/Outfit-Bold.ttf'),
+
+  })
+  return (
+    <ClerkProvider publishableKey="process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY">
+      <SignedIn>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={
+          {headerShown : false}
+        } />
+      </Stack>
+      </SignedIn>
+
+      <SignedOut>
+        <LoginScreen/>
+      </SignedOut>
+      
+    </ClerkProvider>
+  );
+}
+
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -23,4 +23,6 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+
+  PRIMARY: '#47bf39'
 };
